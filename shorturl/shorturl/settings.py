cf31322d76ccc81254979ffeb922c787a1057d35
Gissehel <public-hg-repo-dantus@gissehel.org>
2016-09-27 05:11:05 +0200

    Seems needed since django 1.10

--- shorturl/shorturl/settings.py
+++ shorturl/shorturl/settings.py
@@ -54,6 +54,19 @@ ROOT_URLCONF = 'shorturl.urls'
 
 WSGI_APPLICATION = 'shorturl.wsgi.application'
 
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': ['templates'],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
+}]
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
